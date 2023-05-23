3facbf35102d9fb5ac8571ca70d0c55e5f02f452
akenith <1781998918@qq.com>
2023-05-23 12:16:10 +0800

    支持跨域请求中台接口

--- src/main/main.window.ts
+++ src/main/main.window.ts
@@ -9,14 +9,14 @@ export async function createWindow() {
         height: 614,
         frame: false, // 无边框
         webPreferences: {
-          //  nodeIntegration: false,
+            nodeIntegration: false,
             contextIsolation: true,
             preload: join(__dirname, '../preload/index.js'),
             devTools: isDev,
             webSecurity: false,// 允许跨域访问
-            nodeIntegration: true, // 允许 Node.js APIs 在渲染进程中使用
-            nodeIntegrationInWorker: true, // 允许 Node.js APIs 在 Web Worker 中使用（仅 Electron 5+）
-            nodeIntegrationInSubFrames: true // 允许 Node.js APIs 在子 frame 中使用（仅 Electron 5+）
+           // nodeIntegration: false, // 允许 Node.js APIs 在渲染进程中使用
+           // nodeIntegrationInWorker: true, // 允许 Node.js APIs 在 Web Worker 中使用（仅 Electron 5+）
+          //  nodeIntegrationInSubFrames: true // 允许 Node.js APIs 在子 frame 中使用（仅 Electron 5+）
         },
         autoHideMenuBar: !isDev,
     })
