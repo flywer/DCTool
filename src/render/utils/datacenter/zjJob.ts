338b9fcdf2719e05b5327f314fcec6fa39c62d6d
akenith <1781998918@qq.com>
2023-07-20 14:59:44 +0800

    数据湖质检任务创建维护

--- src/render/utils/datacenter/zjJob.ts
+++ src/render/utils/datacenter/zjJob.ts
@@ -81,6 +81,7 @@ export const convertZjJson = (jsonStr: string, tableName: string): string => {
     jobJson.crontab = ''
     jobJson = JSON.parse(updateSjkUUID(removeIds(jobJson)))
 
+    const oldSourceTableName = jobJson.dataDevBizVo.qualityInspectionDtoList[0].sourceTableName
     const oldTableAbbr = jobJson.dataDevBizVo.qualityInspectionDtoList[0].sourceTableName.split('_')[1]
 
     jobJson.modelJson = jobJson.modelJson.replaceAll(oldTableAbbr, 'depart')
@@ -89,7 +90,7 @@ export const convertZjJson = (jsonStr: string, tableName: string): string => {
     const newAimTableName = `di_depart_${tableName.toLowerCase()}_right_dwd`
     const newWrongTableName = `di_depart_${tableName.toLowerCase()}_error_dwd`
 
-    jobJson.dataDevBizVo.qualityInspectionDtoList[0].incrementColumnName = `${newSourceTableName}.cd_time`
+    jobJson.dataDevBizVo.qualityInspectionDtoList[0].incrementColumnName = `cd_time`
     jobJson.dataDevBizVo.qualityInspectionDtoList[0].incrementColumnValueInit = "2023-01-01T03:33:44.132Z"
 
     jobJson.dataDevBizVo.qualityInspectionDtoList[0].qualityInspectionFieldList = jobJson.dataDevBizVo.qualityInspectionDtoList[0].qualityInspectionFieldList.map(obj => {
@@ -103,10 +104,13 @@ export const convertZjJson = (jsonStr: string, tableName: string): string => {
     jobJson.dataDevBizVo.qualityInspectionDtoList[0].wrongTableName = newWrongTableName
 
     //替换关联表里的表名，但关联的是基础数据的不用替换
-    jobJson.dataDevBizVo.qualityInspectionDtoList[0].qualityInspectionFieldList.forEach((field: any) => {
-        field.ruleList.forEach((rule: any) => {
+    jobJson.dataDevBizVo.qualityInspectionDtoList[0].qualityInspectionFieldList.forEach((inspField: any) => {
+
+        inspField.field = inspField.field.replace(`${oldSourceTableName}.`, '')
+
+        inspField.ruleList.forEach((rule: any) => {
             if (rule.customSqlKey != undefined) {
-                rule.customSqlKey = rule.customSqlKey.replaceAll(oldTableAbbr, 'depart');
+                rule.customSqlKey = rule.customSqlKey.replaceAll(`${oldSourceTableName}.`, '');
             }
             if (rule.fromTableDataTable != undefined && !isBasicTable(rule.fromTableDataTable)) {
                 rule.fromTableDataTable = rule.fromTableDataTable.replaceAll(oldTableAbbr, 'depart');
@@ -121,7 +125,7 @@ export const convertZjJson = (jsonStr: string, tableName: string): string => {
     return JSON.stringify(jobJson, null, 2)
 }
 
-const convertCustomSqlTableName = (sql: string, oldTableAbbr: string, newTableAbbr: string) => {
+export const convertCustomSqlTableName = (sql: string, oldTableAbbr: string, newTableAbbr: string) => {
     // 定义正则表达式匹配规则，匹配 xzzf_ods. 后面的表名
     const pattern = /(?<=xzzf_ods\.)\w+/g;
 
@@ -136,5 +140,108 @@ const convertCustomSqlTableName = (sql: string, oldTableAbbr: string, newTableAb
         }
     }
     return newSql
+}
+
+const dataLakeZjJsonTemplate = {
+    code: 'sjkf9799847f2754603bb0a021dadaf56ee',
+    modelXml: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<definitions xmlns=\"http://www.omg.org/spec/BPMN/20100524/MODEL\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:flowable=\"http://flowable.org/bpmn\" xmlns:bpmndi=\"http://www.omg.org/spec/BPMN/20100524/DI\" xmlns:omgdc=\"http://www.omg.org/spec/DD/20100524/DC\" xmlns:omgdi=\"http://www.omg.org/spec/DD/20100524/DI\" typeLanguage=\"http://www.w3.org/2001/XMLSchema\" expressionLanguage=\"http://www.w3.org/1999/XPath\" targetNamespace=\"http://www.flowable.org/processdef\" exporter=\"Flowable Open Source Modeler\" exporterVersion=\"6.7.2\">\n    <process id=\"sjkf9799847f2754603bb0a021dadaf56ee\" name=\"sjkf9799847f2754603bb0a021dadaf56ee\" isExecutable=\"true\">\n        <userTask id=\"sjkab701d7ddcc14bee843af2257c94b4f8\" name=\"数据质检\" flowable:formFieldValidation=\"true\">\n            <extensionElements>\n                <flowable:taskListener event=\"create\" delegateExpression=\"${dataQuInTaskListener}\"/>\n            </extensionElements>\n        </userTask>\n        <startEvent id=\"sjk01f6b82b3ca54b229be390f243608b20\" name=\"开始\" flowable:formFieldValidation=\"true\"/>\n        <endEvent id=\"sjk46eb79fe11f54659b43cb186e1c28d34\" name=\"结束\"/>\n        <sequenceFlow id=\"sjk19049623d99e41648e64d9af1d220c48\" name=\"MySQL\" sourceRef=\"sjkab701d7ddcc14bee843af2257c94b4f8\" targetRef=\"sjk46eb79fe11f54659b43cb186e1c28d34\"/>\n        <sequenceFlow id=\"sjk2fb3fcb6759e48c69ea49f8c912c8852\" name=\"MySQL\" sourceRef=\"sjk01f6b82b3ca54b229be390f243608b20\" targetRef=\"sjkab701d7ddcc14bee843af2257c94b4f8\"/>\n    </process>\n</definitions>",
+    modelJson: "{\"nodeList\":[{\"id\":\"sjkfc60ee0f505540c1ac10cc0a9a52a11c\",\"shape\":\"image\",\"image\":\"/szrzyt/data_center/tdbs-dev/48fd225e7111088563b7805c805a7e57.svg\",\"size\":\"20\",\"type\":\"database\",\"database\":\"MySQL\",\"name\":\"MySQL\",\"databaseName\":12,\"tableName\":\"sourceTableName\"},{\"id\":\"sjkab701d7ddcc14bee843af2257c94b4f8\",\"shape\":\"image\",\"image\":\"/szrzyt/data_center/tdbs-dev/38522f61e19372817833d4ee81775de7.svg\",\"size\":\"20\",\"delegateExpression\":\"dataQuInTaskListener\",\"type\":\"component\",\"name\":\"数据质检\"},{\"id\":\"sjk9f8e62d347c9495c982f7122500b6539\",\"shape\":\"image\",\"image\":\"/szrzyt/data_center/tdbs-dev/48fd225e7111088563b7805c805a7e57.svg\",\"size\":\"20\",\"type\":\"database\",\"database\":\"MySQL\",\"name\":\"MySQL\",\"databaseName\":8,\"tableName\":\"aimTableName\"},{\"id\":\"sjk37950af2f66e46d8addfad6a0dbec92f\",\"shape\":\"image\",\"image\":\"/szrzyt/data_center/tdbs-dev/32a601d50ea448553386f286a6911239.svg\",\"size\":\"20\",\"type\":\"database\",\"database\":\"TDBS-Hive\",\"name\":\"TDBS-Hive\",\"databaseName\":6,\"tableName\":\"wrongTableName\"},{\"id\":\"sjk01f6b82b3ca54b229be390f243608b20\",\"shape\":\"image\",\"image\":\"/szrzyt/data_center/tdbs-dev/65accb422a8d3f181bbbc1c537006cc0.svg\",\"size\":\"20\",\"type\":\"startProcess\",\"name\":\"开始\"},{\"id\":\"sjk46eb79fe11f54659b43cb186e1c28d34\",\"shape\":\"image\",\"image\":\"/szrzyt/data_center/tdbs-dev/198b2349289796a4476408838a50f944.svg\",\"size\":\"20\",\"type\":\"endProcess\",\"name\":\"结束\"}],\"edgesList\":[{\"from\":\"sjkfc60ee0f505540c1ac10cc0a9a52a11c\",\"to\":\"sjkab701d7ddcc14bee843af2257c94b4f8\",\"id\":\"sjk2fb3fcb6759e48c69ea49f8c912c8852\"},{\"from\":\"sjkab701d7ddcc14bee843af2257c94b4f8\",\"to\":\"sjk9f8e62d347c9495c982f7122500b6539\",\"id\":\"sjka9843e8d518f4b5bb8aac3bafbaf29bc\"},{\"from\":\"sjkab701d7ddcc14bee843af2257c94b4f8\",\"to\":\"sjk37950af2f66e46d8addfad6a0dbec92f\",\"id\":\"sjkd3fb1715ed6e412ea686cca01ec39eeb\"},{\"from\":\"sjk01f6b82b3ca54b229be390f243608b20\",\"to\":\"sjkfc60ee0f505540c1ac10cc0a9a52a11c\",\"id\":\"sjk3837e0a1d6c649f3a4ca7a5cb76ebfef\"},{\"from\":\"sjk37950af2f66e46d8addfad6a0dbec92f\",\"to\":\"sjk46eb79fe11f54659b43cb186e1c28d34\",\"id\":\"sjk5732749975764615a84075496774c269\"},{\"from\":\"sjk9f8e62d347c9495c982f7122500b6539\",\"to\":\"sjk46eb79fe11f54659b43cb186e1c28d34\",\"id\":\"sjk19049623d99e41648e64d9af1d220c48\"}]}",
+    taskDefKey: "sjkab701d7ddcc14bee843af2257c94b4f8"
+}
+
+export const dataLakeZjJobJsonConvert = (formModel: ZjFormModelType, paramJson: any, tableName: string) => {
+    paramJson.name = `zj_lake_${tableName}`
+
+    paramJson.projectId = formModel.projectId
+    paramJson.projectName = projectIdOptions.find(option => option.value === formModel.projectId).label
+    paramJson.personId = formModel.personId
+    paramJson.personName = personIdOptions.find(option => option.value === formModel.personId).label
+
+    paramJson.code = dataLakeZjJsonTemplate.code
+    paramJson.modelXml = dataLakeZjJsonTemplate.modelXml
+    paramJson.modelJson = dataLakeZjJsonTemplate.modelJson
+    paramJson.dataDevBizVo.qualityInspectionDtoList[0].taskInfoDto.taskDefKey = dataLakeZjJsonTemplate.taskDefKey
+
+    paramJson.dataDevBizVo.qualityInspectionDtoList[0].sourceDbId = '12'
+    paramJson.dataDevBizVo.qualityInspectionDtoList[0].aimDbId = '8'
+    paramJson.dataDevBizVo.qualityInspectionDtoList[0].wrongDbId = '6'
+
+    paramJson.dataDevBizVo.qualityInspectionDtoList[0].incrementColumnName = `cd_time`
+
+    const oldSourceTableName = paramJson.dataDevBizVo.qualityInspectionDtoList[0].sourceTableName
+    // const oldAimTableName = paramJson.dataDevBizVo.qualityInspectionDtoList[0].aimTableName
+    // const oldWrongTableName = paramJson.dataDevBizVo.qualityInspectionDtoList[0].wrongTableName
+
+    const newSourceTableName = `sztk_${tableName}`
+    const newAimTableName = `sztk_${tableName}`
+    const newWrongTableName = `df_lake_${tableName}_error_dm`
+
+    paramJson.dataDevBizVo.qualityInspectionDtoList[0].sourceTableName = newSourceTableName
+    paramJson.dataDevBizVo.qualityInspectionDtoList[0].aimTableName = newAimTableName
+    paramJson.dataDevBizVo.qualityInspectionDtoList[0].wrongTableName = newWrongTableName
+
+    paramJson.modelJson = paramJson.modelJson
+        .replaceAll('sourceTableName', newSourceTableName)
+        .replaceAll('aimTableName', newAimTableName)
+        .replaceAll('wrongTableName', newWrongTableName)
+
+    // 去除id,add_time,cd_time,cd_batch 字段的质检
+    paramJson.dataDevBizVo.qualityInspectionDtoList[0].qualityInspectionFieldList = removeRulesByField(
+        paramJson.dataDevBizVo.qualityInspectionDtoList[0].qualityInspectionFieldList,
+        ['id', 'add_time', 'cd_time', 'cd_batch']
+    )
+
+    // 去除值域质检、外键、身份证、手机号、邮箱、正则质检
+    paramJson.dataDevBizVo.qualityInspectionDtoList[0].qualityInspectionFieldList = removeRulesByIds(
+        paramJson.dataDevBizVo.qualityInspectionDtoList[0].qualityInspectionFieldList,
+        ['2', '5', '7', '8', '9', '16']
+    )
+
+    // 去除自定义sql中包含其他表的质检规则
+    paramJson.dataDevBizVo.qualityInspectionDtoList[0].qualityInspectionFieldList = removeRulesByCustomSql(
+        paramJson.dataDevBizVo.qualityInspectionDtoList[0].qualityInspectionFieldList,
+        'xzzf_ods.'
+    )
+
+    paramJson.dataDevBizVo.qualityInspectionDtoList[0].qualityInspectionFieldList.forEach((inspField: any) => {
+        inspField.ruleList.forEach((rule: any) => {
+            if (rule.fromTableDataSourceId != undefined) {
+                rule.fromTableDataSourceId = 12
+            }
+
+            if (rule.fromTableSourceList != undefined) {
+                rule.fromTableSourceList = [
+                    {
+                        id: 12,
+                        label: "数据湖（TDSQL）"
+                    }
+                ]
+            }
+
+            if (rule.customSqlKey != undefined) {
+                rule.customSqlKey = rule.customSqlKey.replaceAll(`${oldSourceTableName}.`, '');
+            }
+        })
+    })
+
+    return JSON.parse(updateSjkUUID(removeIds(paramJson)))
+}
+
+const removeRulesByField = (list: any, fields: string[]) => {
+    return list.filter(item => !fields.includes(item.field))
+}
+
+const removeRulesByIds = (list: any, ids: string[]) => {
+    return list.filter(item => !ids.includes(item.ruleList[0].inspectionRuleId))
+}
+
+const removeRulesByCustomSql = (list: any, str: string) => {
+    return list.filter(item => {
+        if (item.ruleList[0].customSql != undefined) {
+            return !item.ruleList[0].customSql.includes(str)
+        } else {
+            return true
+        }
+    })
 }
 
--- src/render/views/projectMgt/dataLakeZjTab.vue
+++ src/render/views/projectMgt/dataLakeZjTab.vue
@@ -2,6 +2,14 @@
   <n-scrollbar class="pr-2" style="height: calc(100vh - 95px);" trigger="hover">
     <div class="w-auto h-8 mb-2">
       <n-space inline class="float-right">
+        <n-button secondary type="info" @click="showCreateJobModal">
+          创建任务
+          <template #icon>
+            <n-icon>
+              <Add/>
+            </n-icon>
+          </template>
+        </n-button>
         <n-button secondary strong @click="tableDataInit">
           刷新
           <template #icon>
@@ -45,7 +53,7 @@
   </n-drawer>
 
   <n-modal
-      v-model:show="showModalRef"
+      v-model:show="showValidConfigModalRef"
       :mask-closable="false"
       :closable="true"
       preset="dialog"
@@ -109,17 +117,83 @@
       <n-button :size="'small'" @click="onNegativeClick">返回</n-button>
     </template>
   </n-modal>
+
+  <n-modal
+      v-model:show="showCreateJobModalRef"
+      :mask-closable="false"
+      :closable="true"
+      preset="dialog"
+      role="dialog"
+      :show-icon="false"
+      :title="'创建任务'"
+      :size="'small'"
+      style="width: 570px"
+  >
+    <n-scrollbar class="pr-2" style="max-height: calc(100vh - 300px);" trigger="hover">
+      <n-alert class="mt-4" type="default" :show-icon="false">
+        1.已去除id,add_time,cd_time,cd_batch 字段的质检<br/>
+        2.已去除必填项中值域、主外键一致性、身份证号、手机号、邮箱、正则的质检<br/>
+        3.已去除自定义sql中包含中台表的质检规则
+      </n-alert>
+
+      <n-form
+          class="mt-4"
+          ref="createJobModalFormRef"
+          :model="createJobModalFormModel"
+          :rules="createJobModalFormRules"
+          :size="'small'"
+      >
+        <n-grid :cols="4" :x-gap="4">
+          <n-form-item-gi :span="4" label="项目" path="projectId">
+            <n-select
+                v-model:value="createJobModalFormModel.projectId"
+                placeholder="选择项目"
+                :options="projectIdOptions"
+                :consistent-menu-width="false"
+                filterable
+                disabled
+            />
+          </n-form-item-gi>
+          <n-form-item-gi :span="4" label="表名" path="jobJsonId">
+            <n-select
+                v-model:value="createJobModalFormModel.jobJsonId"
+                placeholder="选择表名"
+                :options="tableNameOptions"
+                :consistent-menu-width="false"
+                filterable
+                :loading="tableSelectLoading"
+            />
+          </n-form-item-gi>
+          <n-form-item-gi :span="4" label="责任人" path="personId">
+            <n-select
+                v-model:value="createJobModalFormModel.personId"
+                placeholder="选择责任人"
+                :options="personIdOptions"
+                :consistent-menu-width="false"
+            />
+          </n-form-item-gi>
+        </n-grid>
+      </n-form>
+    </n-scrollbar>
+    <template #action>
+      <n-button type="primary" :size="'small'" @click="createJob" :loading="isCreating">确定
+      </n-button>
+      <n-button :size="'small'" @click="showCreateJobModalRef=!showCreateJobModalRef">返回</n-button>
+    </template>
+  </n-modal>
 </template>
 
 <script setup lang="ts">
-import {get_table_sql} from "@render/api/auxiliaryDb";
+import {get_table_sql, get_zj_json} from "@render/api/auxiliaryDb";
 import {
-  create_valid_config,
+  add_work_flow, create_table,
+  create_valid_config, get_tables,
   get_valid_config_page,
   get_workflow_log,
   get_workflow_page,
   gte_usrc_org_tree
 } from "@render/api/datacenter";
+import {personIdOptions, projectIdOptions} from "@render/typings/datacenterOptions";
 import {
   getCustomTableValidConfig,
   getWorkflowJobStatus,
@@ -131,9 +205,11 @@ import {
   workflowDelete,
   workflowJobGetLastExecTime,
   workflowJobGetNextExecTime,
-  workflowReRun, workflowRun,
+  workflowReRun,
+  workflowRun,
 } from "@render/utils/datacenter/jobTabUtil";
-import {Refresh} from '@vicons/ionicons5'
+import {dataLakeZjJobJsonConvert} from "@render/utils/datacenter/zjJob";
+import {Add, Refresh} from '@vicons/ionicons5'
 import {isEmpty} from "lodash-es";
 import {
   DataTableColumns,
@@ -149,7 +225,7 @@ import {h, onMounted, reactive, ref} from "vue";
 const tableDataRef = ref([])
 const isTableLoading = ref(false)
 
-const projectId = '26'
+const projectId = '39'
 
 onMounted(() => {
   tableDataInit()
@@ -322,6 +398,192 @@ const paginationReactive = reactive({
   }
 })
 
+// region 任务创建
+
+const showCreateJobModalRef = ref(false)
+
+const createJobModalFormRef = ref<FormInst | null>(null);
+const createJobModalFormModel = ref({
+  jobJsonId: '',
+  personId: '',
+  projectId: projectId,
+})
+const createJobModalFormRules = {
+  jobJsonId: {
+    required: true,
+    trigger: ['change'],
+    message: '请选择表名'
+  },
+  projectId: {
+    required: true,
+    trigger: ['change'],
+    message: '请选择项目'
+  },
+  personId: {
+    required: true,
+    trigger: ['change'],
+    message: '请选择责任人'
+  }
+}
+
+const tableNameOptions = ref<Array<SelectOption | SelectGroupOption>>()
+const tableSelectLoading = ref(false)
+
+const isCreating = ref(false)
+const showCreateJobModal = () => {
+  showCreateJobModalRef.value = true
+
+  get_zj_json()
+      .then((res) => {
+        tableNameOptions.value = res?.filter(item => item.zjJson != null).map(
+            (v => ({
+              label: v.tableName,
+              value: v.id.toString(),
+              json: v.zjJson,
+            }))
+        ) || []
+      }).finally(() => tableSelectLoading.value = false)
+}
+
+const createJob = () => {
+  isCreating.value = true
+
+  createJobModalFormRef.value?.validate((errors) => {
+    if (!errors) {
+      let paramJson = JSON.parse(tableNameOptions.value.find(item => item.value === createJobModalFormModel.value.jobJsonId).json as string)
+      let tableName = (tableNameOptions.value.find(item => item.value === createJobModalFormModel.value.jobJsonId).label as string).toLowerCase()
+
+      const newWrongTableName = `df_lake_${tableName}_error_dm`
+
+      // 查询是否已创建此错误表
+      get_tables('6', newWrongTableName).then(res => {
+        if (res.length > 0) {
+          add_work_flow(dataLakeZjJobJsonConvert(createJobModalFormModel.value, paramJson, tableName)).then((res) => {
+            if (res.code == 200) {
+              window.$message.success('质检任务创建成功')
+              tableDataInit()
+              showCreateJobModalRef.value = false
+            } else {
+              window.$message.error(res.message)
+            }
+          }).finally(() => isCreating.value = false)
+        } else {
+          window.$dialog.warning({
+            title: '警告',
+            content: `检测到${newWrongTableName}未创建，是否创建？`,
+            positiveText: '确定',
+            negativeText: '取消',
+            onPositiveClick: async () => {
+              await createWrongTable(tableName).then(() => {
+                add_work_flow(dataLakeZjJobJsonConvert(createJobModalFormModel.value, paramJson, tableName)).then((res) => {
+                  if (res.code == 200) {
+                    window.$message.success('质检任务创建成功')
+                    tableDataInit()
+                    showCreateJobModalRef.value = false
+                  } else {
+                    window.$message.error(res.message)
+                  }
+                }).finally(() => isCreating.value = false)
+              })
+            }
+          })
+        }
+      })
+    } else {
+      isCreating.value = false
+      console.error(errors)
+    }
+  })
+}
+
+const createWrongTable = async (tableName: string) => {
+  let paramsJson = {
+    sourceId: '6',
+    dbType: "tbds-hive",
+    sourceName: "数据中台（TBDS）",
+    dataTierCode: "SJTZ",
+    dataTierName: "数据中台",
+    namedJson: '',
+    datamodelTableFieldsVoList: [
+      {
+        dataType: "",
+        fieldName: "",
+        fieldDescribe: "",
+        fieldSize: "",
+        fieldPrecision: "",
+        emptyFlag: 0,
+        keyFlag: 0,
+        writeFieldSize: true,
+        writeFieldPrecision: true,
+        max: 2,
+        validity: {
+          fieldName: false,
+          fieldDescribe: false,
+          fieldType: false,
+          fieldSize: false,
+          fieldPrecision: false,
+          emptyFlag: false,
+          keyFlag: false
+        }
+      }
+    ],
+    lifeCycle: '1',
+    ddlSql: '',
+    tableName: ''
+  }
+
+  paramsJson.namedJson = JSON.stringify([
+    {
+      id: 1,
+      table: "更新方式",
+      key: "DF",
+      value: "全量表",
+      saveValue: null,
+      flagUser: 1
+    },
+    {
+      id: '1638446496908140546',
+      table: "组织机构",
+      key: 'LAKE',
+      value: '数据湖',
+      saveValue: null,
+      flagUser: 1
+    }, {
+      id: '2',
+      table: "自定义内容",
+      key: `${tableName}_error`,
+      value: null,
+      saveValue: null,
+      flagUser: 0,
+      selectList: []
+    }, {
+      id: "1640177719367512066",
+      table: "数据层级",
+      key: "DM",
+      value: "数据集市层",
+      saveValue: null,
+      flagUser: 1,
+    }
+  ])
+
+  paramsJson.tableName = `df_lake_${tableName}_error_dm`
+
+  const sql = (await get_table_sql({tableName: tableName}))[0].sql
+
+  paramsJson.ddlSql = `CREATE TABLE ${paramsJson.tableName} ${sql}`
+
+  await create_table(paramsJson).then(res => {
+    if (res.success && res.code == 200) {
+      window.$message.success(res.message)
+    } else {
+      window.$message.error(res.message)
+    }
+  })
+
+}
+
+//endregion
+
 // region 日志
 const showDrawerRef = ref(false)
 const logItemsRef = ref<TimelineItemProps[]>([])
@@ -372,7 +634,7 @@ const showWorkflowLog = async (v) => {
 //endregion
 
 //region 质检配置管理
-const showModalRef = ref(false)
+const showValidConfigModalRef = ref(false)
 const isValidConfigRef = ref(false)
 
 // 组织机构下拉值
@@ -439,7 +701,7 @@ const showValidConfigModal = async (v) => {
     value: v.id
   })))
 
-  showModalRef.value = true
+  showValidConfigModalRef.value = true
 }
 
 const customSort = (a: any, b: any) => {
@@ -472,18 +734,19 @@ const onPositiveClick = () => {
           }
         }).finally(() => {
           isSaving.value = false
-          showModalRef.value = false
+          showValidConfigModalRef.value = false
         })
       }
 
     } else {
+      isSaving.value = false
       console.error(errors)
     }
   })
 }
 
 const onNegativeClick = () => {
-  showModalRef.value = false
+  showValidConfigModalRef.value = false
 }
 
 const handlemechanismIdUpdate = () => {
