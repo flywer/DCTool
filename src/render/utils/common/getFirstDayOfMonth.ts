6c9060ce4b71ac3f1bf2b5d9865babf0f20c09af
akenith <1781998918@qq.com>
2023-08-18 15:55:49 +0800

    ✨ Feature: 获取当前星期的星期一

--- src/render/utils/common/getFirstDayOfMonth.ts
+++ src/render/utils/common/getFirstDayOfMonth.ts
@@ -6,3 +6,17 @@ export const getFirstDayOfMonth = (): Date => {
     // 使用当前年份和月份创建一个新的 Date 对象，将日期设置为 1 来获取该月的第一天
     return new Date(year, month, 1);
 }
+
+export const getMondayOfCurrentWeek = (): Date => {
+    const currentDate = new Date();
+    const currentDayOfWeek = currentDate.getDay(); // 0 (Sunday) to 6 (Saturday)
+
+    // Calculate the number of days to subtract to get to the previous Monday
+    const daysToSubtract = currentDayOfWeek === 0 ? 6 : currentDayOfWeek - 1;
+
+    const mondayDate = new Date(currentDate);
+    mondayDate.setDate(currentDate.getDate() - daysToSubtract);
+    mondayDate.setHours(0, 0, 0, 0);
+
+    return mondayDate;
+}
