eeb5f65a5ca4b7328ece96dd59800d12df966f74
akenith <1781998918@qq.com>
2023-10-07 11:37:07 +0800

    🐛 Fix: 日期字符串生成时空值错误

--- src/render/utils/common/dateUtils.ts
+++ src/render/utils/common/dateUtils.ts
@@ -1,12 +1,19 @@
-export const formatDate = (date: Date): string => {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
+import {isNull} from "lodash";
 
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+export const formatDate = (date: Date): string => {
+    if (!isNull(date)) {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    } else {
+        return null
+    }
+
 }
 
 export const isValidDateString = (dateString: string): boolean => {
