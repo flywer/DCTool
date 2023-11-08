2f7fc7777ba51a0bcef9702b8368d2976ccbc5d1
akenith <1781998918@qq.com>
2023-11-08 16:02:28 +0800

    🔨 Refactor: vue-clipboard3特殊情况失效，新增electron内置复制方法

--- src/render/utils/common/clipboard.ts
+++ src/render/utils/common/clipboard.ts
@@ -1,3 +1,4 @@
+import {clipboard_write_text} from "@render/api/app.api";
 import useClipboard from 'vue-clipboard3';
 
 const {toClipboard} = useClipboard();
@@ -5,6 +6,20 @@ const {toClipboard} = useClipboard();
 export const copyText = async (text: string, showMsg?: boolean) => {
     try {
         await toClipboard(text);
+        if (showMsg == undefined) {
+            showMsg = true
+        }
+        if (showMsg) {
+            window.$message.success('复制成功');
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export  const nativeCopyText = async (text: string, showMsg?: boolean) => {
+    try {
+        await clipboard_write_text(text);
         if (showMsg == undefined) {
             showMsg = true
         }
