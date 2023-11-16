f90bb3d8a8522c4ad3f2085fc0681f5ba97a27b7
akenith <1781998918@qq.com>
2023-11-16 10:25:43 +0800

    🔨 Refactor: 优化质检任务配置组件

---
+++ src/render/components/datacenter/zjJobInspSituationModal.vue
@@ -0,0 +1,54 @@
+<template>
+  <n-modal
+      v-model:show="_show"
+      :mask-closable="false"
+      :closable="true"
+      preset="dialog"
+      role="dialog"
+      :show-icon="false"
+      :title="'质检情况'"
+      :size="'small'"
+      style="width: 750px"
+  >
+    <job-inspection-tab :inps-table-name="inspTableName" :inps-table-db-id="inspTableDbId"/>
+  </n-modal>
+</template>
+
+<script setup lang="ts">
+import JobInspectionTab from "@render/views/jobMgt/projectTree/jobInspectionTab.vue";
+import {ref, watch} from "vue";
+
+const props = defineProps({
+  show: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  inspTableName: {
+    type: String,
+    required: true,
+    default: null
+  },
+  inspTableDbId: {
+    type: String,
+    required: true,
+    default: null
+  }
+})
+
+const emit = defineEmits(['update:show', 'onAfterLeave'])
+
+const _show = ref(false)
+
+watch(() => props.show, (v) => {
+  _show.value = v
+})
+watch(_show, (v) => {
+  emit('update:show', v)
+})
+
+</script>
+
+<style scoped>
+
+</style>
