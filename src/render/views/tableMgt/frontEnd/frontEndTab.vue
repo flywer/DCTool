837d3acb8de5ed59893861ba62c3abc487b7326b
akenith <1781998918@qq.com>
2024-01-04 14:32:40 +0800

    🔨 Refactor: 代码优化

--- src/render/views/tableMgt/frontEnd/frontEndTab.vue
+++ src/render/views/tableMgt/frontEnd/frontEndTab.vue
@@ -4,19 +4,11 @@
       <n-tab-pane name="1" tab="表名关联">
         <table-name-association/>
       </n-tab-pane>
-      <n-tab-pane name="2" tab="行为数据字段情况查询">
-        <action-data-problem-query/>
-      </n-tab-pane>
-      <n-tab-pane name="3" tab="行为数据统计查询">
-        <action-data-sum-stat/>
-      </n-tab-pane>
     </n-tabs>
   </n-layout>
 </template>
 
 <script setup lang="ts">
-import ActionDataProblemQuery from "@render/views/tableMgt/frontEnd/actionDataProblemQuery.vue";
-import ActionDataSumStat from "@render/views/tableMgt/frontEnd/actionDataSumStat.vue";
 import TableNameAssociation from "@render/views/tableMgt/frontEnd/tableNameAssociation.vue";
 </script>
 
